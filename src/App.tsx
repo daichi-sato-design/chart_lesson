c8082fbe542931ca2b6d451b0169f804618dcb3e
daichi-sato-design <daichisato.design@gmail.com>
2021-12-25 14:46:45 +0900

    implemant bar plot

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-import LinePlot from './components/LinePlot';
-import PiePlot from './components/PiePlot';
+import LinePlot from './components/LinePlot'
+import PiePlot from './components/PiePlot'
+import BarPlot from './components/BarPlot'
 
 function App() {
-  return <div className="App">
-    <LinePlot />
-    <PiePlot/>
-  </div>;
+  return (
+    <div className="App">
+      <div className="App__wrap">
+        <LinePlot />
+        <PiePlot />
+        <BarPlot />
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
